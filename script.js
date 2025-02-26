35e2eb07546cc13660f93889ce2b8f28fbaa2cc7
yadiel <yadield2889@tuckahoeschools.org>
2025-02-26 13:31:59 +0000

    idk what im doin

---
+++ script.js
@@ -0,0 +1,12 @@
+let calculatorDisplay = document.querySelector(".display");
+let input = document.querySelector(".buttons");
+let solveMode = 0;
+let answer = document.querySelector(".answer");
+
+// making the input into a calculations
+let calculateInput  = () => {
+    input.addEventListener('click', () => {  //adding click activated display 
+        event.target.textContent; // saves the value when button is clicked
+        answer.textContent = input 
+})}     
+
