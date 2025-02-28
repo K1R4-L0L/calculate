b04ced31a72f92c6588c686642c13048750df7c4
yadiel <yadield2889@tuckahoeschools.org>
2025-02-28 13:32:34 +0000

    LETS DEBUG THIS TOEGTHER

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let calculatorDisplay = document.querySelector(".display");
 let input = document.querySelector(".buttons");
 let solveMode = 0;
-let answer = document.querySelector(".answer");
+document.querySelector("#answer")
 
 // making the input into a calculations
 let calculateInput  = () => {
     input.addEventListener('click', (event) => { calculatorDisplay  //adding click activated display 
-        calculatorDisplay.textContent = event.target.textContent; // saves the value when button is clicked
-        answer.textContent = input 
+    calculatorDisplay.textContent = event.target.textContent; // saves the value when button is clicked
+    calculateInput();
 })}     
 
